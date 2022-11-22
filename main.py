d9a3a69683c28ad15e40d9e780ebf2c5ca08f701
FosterAguilar <113448127+FosterAguilar@users.noreply.github.com>
2022-11-22 15:37:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#This is my first python program
+"""
+
+This code is writtten on python 3.8
+interpreted by the console
+"""
+
+print("Hello World")
+print (50)
+print (3.10)
+print (50,"letters",3.10)
+print ("hello", end = " ")
+print ("world ")
